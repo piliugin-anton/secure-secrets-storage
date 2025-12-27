be1819096869d981a494bbd6ca39bbcc1cc47535
Anton Piliugin <anton.piliugin@icloud.com>
2025-12-28 04:24:31 +0500

    Version print

--- src/main.rs
+++ src/main.rs
@@ -276,7 +276,7 @@ fn main() -> io::Result<()> {
 }
 
 fn print_usage() {
-    println!("Secure Password Vault v2.0");
+    println!("Secure Password Vault v{VERSION}.0");
     println!("\nUsage:");
     println!("  add <key>           - Add a secret");
     println!("  get <key>           - Get a secret");
